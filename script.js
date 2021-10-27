24f1e518cb5386c1c505855b3651a9bf58d21bfd
Duc Van <ducvanit9697@gmail.com>
2021-10-27 16:08:49 +0700

    Slider part 2

--- script.js
+++ script.js
@@ -23,3 +23,12 @@ attachToggleClassForElement(".close-contact-form", ".newsletter-container", "sho
 attachToggleClassForElement(".search-icon", ".search-bg", "show");
 attachToggleClassForElement(".search-bg .close-element", ".search-bg", "show");
 attachToggleClassForElement(".close-search", ".search-bg", "show");
+var headerSwiper = new Swiper(".slider-container", {
+    loop: true,
+    spaceBetween: 30,
+    effect: "fade",
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+});
--- script.ts
+++ script.ts
@@ -41,3 +41,13 @@ attachToggleClassForElement(
 attachToggleClassForElement(".search-icon", ".search-bg", "show");
 attachToggleClassForElement(".search-bg .close-element", ".search-bg", "show");
 attachToggleClassForElement(".close-search", ".search-bg", "show");
+
+let headerSwiper = new Swiper(".slider-container", {
+  loop: true,
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 10000,
+    disableOnInteraction: false,
+  },
+});
